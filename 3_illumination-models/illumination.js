8e012c86bc2301b3b01f5ed767f625e5e8ae26a5
francisccdn <francisccdn@gmail.com>
2021-10-29 15:02:13 -0300

    unfinished gouraud

--- 3_illumination-models/illumination.js
+++ 3_illumination-models/illumination.js
@@ -11,83 +11,64 @@ Illumination Models Template by ICG-UFPB on codepen.io - https://codepen.io/ICG-
 const gouraud = { vertexShader: '', fragmentShader: '' };
 
 gouraud.vertexShader = `
-#pragma vscode_glsllint_stage : STAGE
-
-// 'uniform' contendo informação sobre a fonte de luz ambiente.
+// 'uniform' for ambient light data
 uniform vec3 Ia;
 
-// 'uniforms' contendo informações sobre a fonte de luz pontual.
+// 'uniforms' for point light data
 uniform vec3 Ip_position;
 uniform vec3 Ip_diffuse_color;
 
-// 'uniforms' contendo informações sobre as reflectâncias do objeto.
+// 'uniforms' for reflection constants
 uniform vec3 k_a;
 uniform vec3 k_d;
 uniform vec3 k_s;
 
-// 'I' : Variável que armazenará a cor final (i.e. intensidade) do vértice, após a avaliação do modelo local de iluminação.
-//     A variável 'I' é do tipo 'varying', ou seja, seu valor será calculado pelo Vertex Shader (por vértice)
-//     e será interpolado durante a rasterização das primitivas, ficando disponível para cada fragmento gerado pela rasterização.
+// Final color (i.e. intensity) of vertex, processed by illumination model
 varying vec4 I;
 
-// Programa principal do Vertex Shader.
 void main() {
-
-    // 'modelViewMatrix' : variável de sistema que contém a matriz ModelView (4x4).
-    // 'Ip_pos_cam_spc' : variável que armazenará a posição da fonte de luz no Espaço da Câmera.
-    
+    // Point light position in camera space
     vec4 Ip_pos_cam_spc = modelViewMatrix * vec4(Ip_position, 1.0);
 
-    // 'position' : variável de sistema que contém a posição do vértice (vec3) no espaço do objeto.
-    // 'P_cam_spc' : variável que contém o vértice (i.e. 'position') transformado para o Espaço de Câmera.
-    //     Observe que 'position' é um vetor 3D que precisou ser levado para o espaço projetivo 4D 
-    //     (i.e., acrescentando-se uma coordenada adicional w = 1.0) para poder ser multiplicado pela
-    //     matriz 'modelViewMatrix' (que é 4x4).
-    
+    // Vertex position in camera space
     vec4 P_cam_spc = modelViewMatrix * vec4(position, 1.0);
 
-    // 'normal' : variável de sistema que contém o vetor normal do vértice (vec3) no espaço do objeto.
-    // 'normalMatrix' : variável de sistema que contém a matriz de normais (3x3) gerada a partir da matriz 'modelViewMatrix'.
-    
+    // Vertex normal in camera space
     vec3 N_cam_spc = normalize(normalMatrix * normal);
 
-    // 'normalize()' : função do sistema que retorna o vetor de entrada normalizado (i.e. com comprimento = 1).
-    // 'L_cam_spc' : variável que contém o vetor unitário, no Espaço de Câmera, referente à fonte de luz.
-    
+    // Direction from vertex to point light normalized and in camera space
     vec3 L_cam_spc = normalize(Ip_pos_cam_spc.xyz - P_cam_spc.xyz);
 
-    // 'reflect()' : função do sistema que retorna 'R_cam_spc', isto é, o vetor 'L_cam_spc' refletido 
-    //     em relação o vetor 'N_cam_spc'.
-    
+    // Direction from vertex to point light reflected over vertex normal
     vec3 R_cam_spc = reflect(L_cam_spc, N_cam_spc);
 
-    ///////////////////////////////////////////////////////////////////////////////
-    //
-    // Escreva aqui o seu código para implementar os modelos de iluminação com 
-    // Gouraud Shading (interpolação por vértice). 
-    //
-    ///////////////////////////////////////////////////////////////////////////////
+    // Direction from vertex to camera normalized and in camera space
+    vec3 V_cam_spc = normalize(-P_cam_spc.xyz);
 
-    // 'I' : cor final (i.e. intensidade) do vértice.
-    //     Neste caso, a cor retornada é vermelho. Para a realização do exercício, o aluno deverá atribuir a 'I' o valor
-    //     final gerado pelo modelo local de iluminação implementado.
-    
-    I = vec4(1, 0, 0, 1); 
+    // Highlight factor
+    float n_highlight = 16.0;
 
-    // 'gl_Position' : variável de sistema que conterá a posição final do vértice transformado pelo Vertex Shader.
-    
+    // ** Phong Illumination Model ** //
+
+    vec3 ambient_term = Ia.xyz * k_a.xyz;
+    vec3 diffuse_term = Ip_diffuse_color.xyz * k_d.xyz * max(0.0, dot(N_cam_spc, L_cam_spc));
+    vec3 specular_term = Ip_diffuse_color.xyz * k_s.xyz * pow(max(0.0, dot(R_cam_spc, V_cam_spc)), n_highlight);
+
+    vec3 I_3d = ambient_term + diffuse_term + specular_term;
+    I = vec4(I_3d, 1);
+
+    // **** //
+
+    // 'gl_Position' : variável de sistema que conterá a posição final do vértice transformado pelo Vertex Shader.    
     gl_Position = projectionMatrix * P_cam_spc;
 }
 `;
 
 gouraud.fragmentShader = `
-// 'I' : valor de cor originalmente calculada pelo Vertex Shader, e já interpolada para o fragmento corrente.
+// Color processed by illumination model and interpolated for each fragment
 varying vec4 I;
 
-// Programa principal do Fragment Shader.
 void main() {
-
-    // 'gl_FragColor' : variável de sistema que conterá a cor final do fragmento calculada pelo Fragment Shader.
     gl_FragColor = I;
 }
 `;
@@ -96,16 +77,17 @@ void main() {
 const phong = { vertexShader: '', fragmentShader: '' };
 
 phong.vertexShader = `
-#pragma vscode_glsllint_stage : STAGE
-
 // 'uniform' contendo informação sobre a fonte de luz ambiente.
+    
 uniform vec3 Ia;
-
+    
 // 'uniforms' contendo informações sobre a fonte de luz pontual.
+
 uniform vec3 Ip_position;
 uniform vec3 Ip_diffuse_color;
 
 // 'uniforms' contendo informações sobre as reflectâncias do objeto.
+
 uniform vec3 k_a;
 uniform vec3 k_d;
 uniform vec3 k_s;
@@ -113,9 +95,11 @@ uniform vec3 k_s;
 // 'I' : Variável que armazenará a cor final (i.e. intensidade) do vértice, após a avaliação do modelo local de iluminação.
 //     A variável 'I' é do tipo 'varying', ou seja, seu valor será calculado pelo Vertex Shader (por vértice)
 //     e será interpolado durante a rasterização das primitivas, ficando disponível para cada fragmento gerado pela rasterização.
+
 varying vec4 I;
 
 // Programa principal do Vertex Shader.
+
 void main() {
 
     // 'modelViewMatrix' : variável de sistema que contém a matriz ModelView (4x4).
@@ -146,19 +130,24 @@ void main() {
     
     vec3 R_cam_spc = reflect(L_cam_spc, N_cam_spc);
 
-    ///////////////////////////////////////////////////////////////////////////////
-    //
-    // Escreva aqui o seu código para implementar os modelos de iluminação com 
+    
     // Gouraud Shading (interpolação por vértice). 
     //
-    ///////////////////////////////////////////////////////////////////////////////
-
     // 'I' : cor final (i.e. intensidade) do vértice.
     //     Neste caso, a cor retornada é vermelho. Para a realização do exercício, o aluno deverá atribuir a 'I' o valor
     //     final gerado pelo modelo local de iluminação implementado.
-    
-    I = vec4(1, 0, 0, 1); 
 
+    // Termo Ambiente
+    vec4 tA = vec4(Ia,1) * vec4(k_a,1);
+    // Termo Difuso
+    vec4 tD = (vec4(Ip_diffuse_color,1) * vec4(k_d,1)) * max(0.0,dot(N_cam_spc,L_cam_spc));
+    // Termo Especular
+    float expoente_n = 20.0;
+    vec3 vision = -normalize(vec3(P_cam_spc)); // adotando a câmera como referencial
+    vec4 tE = (vec4(Ip_diffuse_color,1) * vec4(k_s,1)) * pow(max(0.0,dot(R_cam_spc,vision)), expoente_n);
+    
+    I =  tA +  tD + tE;
+    
     // 'gl_Position' : variável de sistema que conterá a posição final do vértice transformado pelo Vertex Shader.
     
     gl_Position = projectionMatrix * P_cam_spc;
