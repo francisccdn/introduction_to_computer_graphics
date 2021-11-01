9a3d70b983f7ce6314d2b400da2a0f658a7897b3
francisccdn <francisccdn@gmail.com>
2021-11-01 12:34:38 -0300

    phong shading

--- 3_illumination-models/illumination.js
+++ 3_illumination-models/illumination.js
@@ -59,7 +59,7 @@ void main() {
 
     // **** //
 
-    // 'gl_Position' : variável de sistema que conterá a posição final do vértice transformado pelo Vertex Shader.    
+    // Final vertex position after transformations in shader
     gl_Position = projectionMatrix * P_cam_spc;
 }
 `;
@@ -77,95 +77,74 @@ void main() {
 const phong = { vertexShader: '', fragmentShader: '' };
 
 phong.vertexShader = `
-// 'uniform' contendo informação sobre a fonte de luz ambiente.
-    
-uniform vec3 Ia;
-    
-// 'uniforms' contendo informações sobre a fonte de luz pontual.
-
+// 'uniform' var for point light data
 uniform vec3 Ip_position;
-uniform vec3 Ip_diffuse_color;
 
-// 'uniforms' contendo informações sobre as reflectâncias do objeto.
-
-uniform vec3 k_a;
-uniform vec3 k_d;
-uniform vec3 k_s;
-
-// 'I' : Variável que armazenará a cor final (i.e. intensidade) do vértice, após a avaliação do modelo local de iluminação.
-//     A variável 'I' é do tipo 'varying', ou seja, seu valor será calculado pelo Vertex Shader (por vértice)
-//     e será interpolado durante a rasterização das primitivas, ficando disponível para cada fragmento gerado pela rasterização.
-
-varying vec4 I;
-
-// Programa principal do Vertex Shader.
+// 'varying' vars for vectors needed in computation
+varying vec4 Ip_pos_cam_spc;
+varying vec4 P_cam_spc;
+varying vec3 N_cam_spc;
+varying vec3 L_cam_spc;
+varying vec3 R_cam_spc;
+varying vec3 V_cam_spc;
 
 void main() {
+    // Point light position in camera space
+    Ip_pos_cam_spc = modelViewMatrix * vec4(Ip_position, 1.0);
 
-    // 'modelViewMatrix' : variável de sistema que contém a matriz ModelView (4x4).
-    // 'Ip_pos_cam_spc' : variável que armazenará a posição da fonte de luz no Espaço da Câmera.
-    
-    vec4 Ip_pos_cam_spc = modelViewMatrix * vec4(Ip_position, 1.0);
+    // Vertex position in camera space
+    P_cam_spc = modelViewMatrix * vec4(position, 1.0);
 
-    // 'position' : variável de sistema que contém a posição do vértice (vec3) no espaço do objeto.
-    // 'P_cam_spc' : variável que contém o vértice (i.e. 'position') transformado para o Espaço de Câmera.
-    //     Observe que 'position' é um vetor 3D que precisou ser levado para o espaço projetivo 4D 
-    //     (i.e., acrescentando-se uma coordenada adicional w = 1.0) para poder ser multiplicado pela
-    //     matriz 'modelViewMatrix' (que é 4x4).
-    
-    vec4 P_cam_spc = modelViewMatrix * vec4(position, 1.0);
+    // Vertex normal in camera space
+    N_cam_spc = normalize(normalMatrix * normal);
 
-    // 'normal' : variável de sistema que contém o vetor normal do vértice (vec3) no espaço do objeto.
-    // 'normalMatrix' : variável de sistema que contém a matriz de normais (3x3) gerada a partir da matriz 'modelViewMatrix'.
-    
-    vec3 N_cam_spc = normalize(normalMatrix * normal);
+    // Direction from vertex to point light normalized and in camera space
+    L_cam_spc = normalize(Ip_pos_cam_spc.xyz - P_cam_spc.xyz);
 
-    // 'normalize()' : função do sistema que retorna o vetor de entrada normalizado (i.e. com comprimento = 1).
-    // 'L_cam_spc' : variável que contém o vetor unitário, no Espaço de Câmera, referente à fonte de luz.
-    
-    vec3 L_cam_spc = normalize(Ip_pos_cam_spc.xyz - P_cam_spc.xyz);
+    // Direction from vertex to point light reflected over vertex normal
+    R_cam_spc = reflect(L_cam_spc, N_cam_spc);
 
-    // 'reflect()' : função do sistema que retorna 'R_cam_spc', isto é, o vetor 'L_cam_spc' refletido 
-    //     em relação o vetor 'N_cam_spc'.
-    
-    vec3 R_cam_spc = reflect(L_cam_spc, N_cam_spc);
+    // Direction from vertex to camera normalized and in camera space
+    V_cam_spc = normalize(P_cam_spc.xyz);
 
-    
-    // Gouraud Shading (interpolação por vértice). 
-    //
-    // 'I' : cor final (i.e. intensidade) do vértice.
-    //     Neste caso, a cor retornada é vermelho. Para a realização do exercício, o aluno deverá atribuir a 'I' o valor
-    //     final gerado pelo modelo local de iluminação implementado.
-
-    // Termo Ambiente
-    vec4 tA = vec4(Ia,1) * vec4(k_a,1);
-    // Termo Difuso
-    vec4 tD = (vec4(Ip_diffuse_color,1) * vec4(k_d,1)) * max(0.0,dot(N_cam_spc,L_cam_spc));
-    // Termo Especular
-    float expoente_n = 20.0;
-    vec3 vision = -normalize(vec3(P_cam_spc)); // adotando a câmera como referencial
-    vec4 tE = (vec4(Ip_diffuse_color,1) * vec4(k_s,1)) * pow(max(0.0,dot(R_cam_spc,vision)), expoente_n);
-    
-    I =  tA +  tD + tE;
-    
-    // 'gl_Position' : variável de sistema que conterá a posição final do vértice transformado pelo Vertex Shader.
-    
-    gl_Position = projectionMatrix * P_cam_spc;
+    // Final vertex position after transformations in shader
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
 `;
 
 phong.fragmentShader = `
-// 'I' : valor de cor originalmente calculada pelo Vertex Shader, e já interpolada para o fragmento corrente.
+// 'uniform' for ambient light data
+uniform vec3 Ia;
 
-varying vec4 I;
+// 'uniforms' for point light data
+uniform vec3 Ip_diffuse_color;
 
-// Programa principal do Fragment Shader.
+// 'uniforms' for reflection constants
+uniform vec3 k_a;
+uniform vec3 k_d;
+uniform vec3 k_s;
+
+// 'varying' vars for vectors needed in computation
+varying vec4 Ip_pos_cam_spc;
+varying vec4 P_cam_spc;
+varying vec3 N_cam_spc;
+varying vec3 L_cam_spc;
+varying vec3 R_cam_spc;
+varying vec3 V_cam_spc;
 
 void main() {
+    // Highlight factor
+    float n_highlight = 16.0;
 
-    // 'gl_FragColor' : variável de sistema que conterá a cor final do fragmento calculada pelo Fragment Shader.
-    
-    gl_FragColor = I;
+    // ** Phong Illumination Model ** //
+
+    vec3 ambient_term = Ia.xyz * k_a.xyz;
+    vec3 diffuse_term = Ip_diffuse_color.xyz * k_d.xyz * max(0.0, dot(N_cam_spc, L_cam_spc));
+    vec3 specular_term = Ip_diffuse_color.xyz * k_s.xyz * pow(max(0.0, dot(R_cam_spc, V_cam_spc)), n_highlight);
+
+    vec3 I = ambient_term + diffuse_term + specular_term;
+
+    gl_FragColor = vec4(I, 1);
 }
 `;
 
@@ -225,10 +204,10 @@ let material = new THREE.ShaderMaterial({
 });
 
 // Vertex Shader
-material.vertexShader = gouraud.vertexShader;
+material.vertexShader = phong.vertexShader;
 
 // Fragment Shader
-material.fragmentShader = gouraud.fragmentShader;
+material.fragmentShader = phong.fragmentShader;
 
 // 'object_mesh' : De acordo com o Three.js, um 'mesh' é a geometria acrescida do material.
 var object_mesh = new THREE.Mesh(geometry, material);
